9bc5b952656c19df3facb448b7cdc6159b095794
sahil <114054220+sahilsangwan-source@users.noreply.github.com>
2024-09-17 16:31:38 +0530

    farmer dashboard update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import Home from "./Home"
 import FDashboard from "./FarmerDashboard"
+
 import "./App.css"
 export default function App(){
   return <>
--- client/src/FarmerDashboard.jsx
+++ client/src/FarmerDashboard.jsx
@@ -3,12 +3,14 @@ import HeroSection from "./components/FarmerHero"
 import GovtSchemes from "./components/FarmerGovt"
 import CompanyAnnouncements from "./components/FarmerCompany"
 import FarmingUpdates from "./components/FarmingTweets"
+import MarketTrends from "./components/MarketTrend"
 export default function FDashboard(){
     return<>
         <NavbarComponent/>
         <HeroSection />
         <GovtSchemes />
         <CompanyAnnouncements/>
+        <MarketTrends/>
         <FarmingUpdates/>
     </>
 }
