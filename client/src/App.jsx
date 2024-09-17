fb321f71970fdbca72c2d86973b9c4bf4e9b13f6
sahil <114054220+sahilsangwan-source@users.noreply.github.com>
2024-09-17 16:41:16 +0530

    nothing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,9 @@
+import Home from "./Home"
+import FDashboard from "./FarmerDashboard"
 
-import Home from "./Home";
-import Login from "./components/Register-Login/Login";
-import Register from "./components/Register-Login/Register";
-
-import FDashboard from "./FarmerDashboard";
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/register" element={<Register />} /> */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/farmer-dashboard" element={<FDashboard />} />
-      </Routes>
-    </Router>
-  );
+import "./App.css"
+export default function App(){
+  return <>
+    <FDashboard/>
+  </>
 }
